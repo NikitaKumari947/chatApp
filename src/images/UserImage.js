fdca7e9156d1a9136b1aa84593be32d4f02b7220
Nikita Kumari <132778281+NikitaKumari947@users.noreply.github.com>
2023-09-08 04:18:31 -0700

    Add files via upload

---
+++ src/images/UserImage.js
@@ -0,0 +1,7 @@
+     export default function UserImage(){
+      const   Image={  
+        data:12,
+        image: "image/User-Profile-PNG.png",
+   
+}
+     }
---
+++ src/reducers/user.js
@@ -0,0 +1,15 @@
+
+
+export default function user(state = {}) {
+ 
+   
+    return {
+      id: 1,
+      name: "Chat User",
+      image: "image/User-Profile-PNG.png",
+      
+   
+  
+    }
+  
+  }
